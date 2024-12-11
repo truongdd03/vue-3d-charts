3986aa0ceea799963b3e8a510c249701200af9dd
truongd1 <truongd1@msu.edu>
2024-12-11 17:57:46 -0500

    package for npm

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import BarChart from "./components/BarChart.vue";
+import type { ChartConfig } from "./types/types";
+
+export {
+  BarChart,
+  type ChartConfig,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
 
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { templateCompilerOptions } from '@tresjs/core';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +15,26 @@ export default defineConfig({
       ...templateCompilerOptions,
     }),
     vueDevTools(),
+    dts({ outDir: 'dist/types', insertTypesEntry: true, tsconfigPath: './tsconfig.app.json', rollupTypes: true })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-3d-charts',
+      fileName: (format) => `vue-3d-charts.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        }
+      }
+    }
+  }
 });
